41b2e690ffd501da32c2a6a4a94b24c3c6421bbf
tatsunode <tatsunode@yahoo.co.jp>
2020-08-07 15:24:27 +0900

    TweetIDをTweetURLから抽出するように変更．TweetIDがTwitterAppからのIntentに直接含まれないようになったため

--- app/src/main/java/dev/tatsuno/tweetclipperserver/ClippingActivity.java
+++ app/src/main/java/dev/tatsuno/tweetclipperserver/ClippingActivity.java
@@ -11,6 +11,10 @@ import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ClippingActivity extends AppCompatActivity {
 
@@ -29,10 +33,10 @@ public class ClippingActivity extends AppCompatActivity {
         String action = intent.getAction();
 
         if (action == null || !action.equals(Intent.ACTION_SEND)) {
+            Toast.makeText(this, "Error: Not Corret Action", Toast.LENGTH_LONG).show();
             // Not Target
-            finish();
+            // finish();
         }
-
         intent.removeExtra("org.chromium.chrome.extra.TASK_ID");
         intent.removeExtra("share_screenshot_as_stream");
 
@@ -40,7 +44,7 @@ public class ClippingActivity extends AppCompatActivity {
 
         ClipData clipData = intent.getClipData();
         ClipData.Item item = clipData.getItemAt(0);
-        String clipItemText = (String) item.getText();
+        // String clipItemText = (String) item.getText();
 
         Log.d(TAG, clipData.toString());
         Log.d(TAG, item.toString());
@@ -52,22 +56,46 @@ public class ClippingActivity extends AppCompatActivity {
             Log.d(TAG, "VALUE: " + extra.toString());
         }
 
-        String tweetId = "";
-        if(extras != null) {
-            CharSequence textChar = extras.getCharSequence(Intent.EXTRA_TEXT);
-            String text = String.valueOf(textChar);
-            if(text != null) {
-                Log.d(TAG, "------");
-                Log.d(TAG, text);
-                Log.d(TAG, "------");
-            }
-            Object tweetIdObj = extras.get("tweet_id");
-            tweetId = tweetIdObj.toString();
+        CharSequence textChar = extras.getCharSequence(Intent.EXTRA_TEXT);
+        String text = String.valueOf(textChar);
+        Log.d(TAG, text);
+
+        String tweetId = getTweetId(text);
+        if (tweetId == null) {
+            Toast.makeText(this, "Error: Cannot find Tweet ID:" + text, Toast.LENGTH_LONG).show();
         }
 
         Uri newUri = Uri.parse(altURL + tweetId);
+        launchEvernote(newUri);
+
+        finish();
+    }
+
+    private String getTweetId(String url) {
+        Log.d(TAG, "INput: " + url);
+        Pattern pattern = Pattern.compile("status/[0-9]+");
+        Matcher matcher = pattern.matcher(url);
+
+        if (matcher.find()) {
+            String matched = matcher.group();
+            String[] splitted = matched.split("/");
+            return splitted[1];
+
+        } else {
+            Log.d(TAG, "Not Matches");
+        }
+        return null;
+    }
+
+    private void launchEvernote(Uri clipTargetUri) {
 
         // Construct Intent to Evernote Clipper
+        Intent evernote = new Intent();
+        evernote.setComponent(new ComponentName("com.evernote", "com.evernote.clipper.ClipActivity"));
+        evernote.putExtra(Intent.EXTRA_TEXT, clipTargetUri.toString());
+        evernote.putExtra(Intent.EXTRA_STREAM, clipTargetUri.toString());
+        startActivity(evernote);
+
         // String[] mimeType = new String[1];
         // mimeType[0] = ClipDescription.MIMETYPE_TEXT_PLAIN;
         // ClipData newClipData = new ClipData(new ClipDescription("text_data", mimeType), new ClipData.Item(newUri));
@@ -75,15 +103,8 @@ public class ClippingActivity extends AppCompatActivity {
         // intent.setComponent(new ComponentName("com.evernote", "com.evernote.clipper.ClipActivity"));
         // startActivity(intent);
 
-        Intent evernote = new Intent();
-        evernote.setComponent(new ComponentName("com.evernote", "com.evernote.clipper.ClipActivity"));
         // evernote.setAction(Intent.ACTION_SEND);
         // evernote.setData(Uri.parse(newUri.toString()));
         // evernote.setType("text/plain");
-        evernote.putExtra(Intent.EXTRA_TEXT, newUri.toString());
-        evernote.putExtra(Intent.EXTRA_STREAM, newUri.toString());
-        startActivity(evernote);
-
-        finish();
     }
 }
